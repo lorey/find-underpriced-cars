5451ccdd5f6ddae92c7337d29057726f80f11364
lorey <git@karllorey.com>
2017-07-01 23:53:37 +0200

    Save single objects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import random
 import urllib
 from datetime import datetime
 from time import sleep
@@ -18,20 +19,22 @@ def main():
 def perform_search():
     cars_data = []
 
-    page = 1
-    while True:
+    for page in range(1, 250):
+        # damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=3500&makeModelVariant1.modelGroupId=20&makeModelVariant1.modelId=73%2C2%2C3%2C4%2C59%2C61%2C5%2C58%2C87&pageNumber=51&scopeId=C&sortOption.sortBy=creationTime&sortOption.sortOrder=DESCENDING
         search_url = 'https://suchen.mobile.de/fahrzeuge/search.html'
         parameters = {
             'ambitCountry': 'DE',
             'damageUnrepaired': 'NO_DAMAGE_UNREPAIRED',
             'isSearchRequest': 'true',
             'makeModelVariant1.makeId': 3500,  # bmw
-            'makeModelVariant1.modelId': 59,  # 123?
+            'makeModelVariant1.modelGroupId': 20,  # 1er
+            'makeModelVariant1.modelId': '73%2C2%2C3%2C4%2C59%2C61%2C5%2C58%2C87',
             'scopeId': 'C',
             'usage': 'USED',
             'sortOption.sortBy': 'creationTime',
             'sortOption.sortOrder': 'DESCENDING',
             'pageNumber': page,
+            'maxPrice': 10000,
         }
 
         url = search_url + '?' + urllib.parse.urlencode(parameters)
@@ -43,10 +46,11 @@ def perform_search():
 
         car_results = soup.find_all('div', {'class': 'cBox-body--resultitem'})
 
-        # return if finished
+        # return if finished early
         if len(car_results) == 0:
             return cars_data
 
+        sleep(5)
         for car_result in car_results:
             car_link = car_result.a
             if car_link.has_attr('data-ad-id'):
@@ -55,12 +59,13 @@ def perform_search():
 
                 sleep(1)
                 print(car_url)
+
                 car_data = crawl_car(car_url)
                 cars_data.append(car_data)
             else:
                 logging.warning('no ad-id: %s' % car_result)
 
-        page += 1
+    return cars_data
 
 
 def crawl_car(url):
@@ -71,6 +76,12 @@ def crawl_car(url):
     html = response.content.decode('utf-8')
 
     car = extract_data_from_car_page(html)
+    car['url'] = url
+
+    filename = 'cars/%d.json' % car['mobile']['ad_id']
+    with open(filename, 'w') as file:
+        file.write(json.dumps(car, ensure_ascii=False, indent=4))
+
     return car
 
 
@@ -80,6 +91,8 @@ def extract_data_from_car_page(html):
     # generic
     title = soup.find('h1', {'id': 'rbt-ad-title'}).get_text()
     price = soup.find('span', {'class': 'rbt-prime-price'}).get_text()
+
+    ad_id = int(soup.find('div', {'class': 'parking-block'})['data-parking'])
 
     # seller
     seller_address = soup.find('p', {'id': 'rbt-seller-address'}).get_text(separator=' ')
@@ -127,6 +140,7 @@ def extract_data_from_car_page(html):
             'seen_at': str(datetime.now()),
         },
         'mobile': {
+            'ad_id': ad_id,
             'title': title,
             'price': price,
             'technical': technical_data,
