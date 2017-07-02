ff9de34ffb7f483d227790bef26582de455e5636
lorey <git@karllorey.com>
2017-07-02 10:36:40 +0200

    car json to row method

--- prediction.py
+++ prediction.py
@@ -23,62 +23,7 @@ def main():
     X = []
     y = []
     for car in cars:
-        row = {}
-
-        # mileage
-        mileage_wo_dot = car['mobile']['technical']['mileage'].replace('.', '')
-        mileage = int(re.findall(r'\d+', mileage_wo_dot)[0])
-        row['mileage'] = mileage
-
-        # times
-        first_registration_raw = car['mobile']['technical']['firstRegistration']
-        first_registration = datetime.strptime(first_registration_raw, '%m/%Y')
-        time_since_registration_in_years = (datetime.now() - first_registration).total_seconds() / (60 * 60 * 24 * 365)
-        row['car_age_in_years'] = time_since_registration_in_years
-
-        row['time_to_hu_in_months'] = -1000
-        if 'hu' in car['mobile']['technical']:
-            hu_raw = car['mobile']['technical']['hu']
-            if hu_raw == 'Neu':
-                time_to_hu = 0.0
-            else:
-                hu = datetime.strptime(hu_raw, '%m/%Y')
-                time_to_hu = (hu - datetime.now()).total_seconds() / (60 * 60 * 24 * 12)
-
-            row['time_to_hu_in_months'] = time_to_hu
-
-        # power
-        power_in_ps = -1
-        if 'power' in car['mobile']['technical']:
-            power_raw = car['mobile']['technical']['power']
-            power_in_ps = int(re.findall(r'\d+', power_raw)[1])
-        else:
-            logging.warning('no power found')
-        row['power_ps'] = power_in_ps
-
-        # previous owners
-        if 'numberOfPreviousOwners' in car['mobile']['technical']:
-            row['owners'] = int(car['mobile']['technical']['numberOfPreviousOwners'])
-
-        # cubic
-        if 'cubicCapacity' in car['mobile']['technical']:
-            cubic_capacity_raw = car['mobile']['technical']['cubicCapacity'].replace('.', '')
-            cubic_capacity = int(re.findall(r'\d+', cubic_capacity_raw)[0])
-            row['cubic_capacity'] = cubic_capacity
-        else:
-            logging.warning('no cubic capacity found')
-
-        # inlining features
-        if car['mobile']['features'] is not None:
-            for feature in car['mobile']['features']:
-                row['features_' + feature] = True
-        else:
-            row['feature_none'] = True
-
-        # tech details as strings
-        excluded_keys = ['mileage', 'firstRegistration', 'hu', 'power', 'cubicCapacity', 'numberOfPreviousOwners']
-        row.update({k: v for k, v in car['mobile']['technical'].items() if k not in excluded_keys})
-
+        row = car_to_row(car)
         X.append(row)
 
         # price
@@ -101,6 +46,67 @@ def main():
         differece = price_actual - price_inferred
         is_cheap = differece < 0
         print(price_actual, price_inferred, differece, is_cheap)
+
+
+def car_to_row(car):
+    row = {}
+
+    # mileage
+    mileage_wo_dot = car['mobile']['technical']['mileage'].replace('.', '')
+    mileage = int(re.findall(r'\d+', mileage_wo_dot)[0])
+    row['mileage'] = mileage
+
+    # times
+    first_registration_raw = car['mobile']['technical']['firstRegistration']
+    first_registration = datetime.strptime(first_registration_raw, '%m/%Y')
+    time_since_registration_in_years = (datetime.now() - first_registration).total_seconds() / (60 * 60 * 24 * 365)
+    row['car_age_in_years'] = time_since_registration_in_years
+
+    # hu/au
+    row['time_to_hu_in_months'] = -1000
+    if 'hu' in car['mobile']['technical']:
+        hu_raw = car['mobile']['technical']['hu']
+        if hu_raw == 'Neu':
+            time_to_hu = 0.0
+        else:
+            hu = datetime.strptime(hu_raw, '%m/%Y')
+            time_to_hu = (hu - datetime.now()).total_seconds() / (60 * 60 * 24 * 12)
+
+        row['time_to_hu_in_months'] = time_to_hu
+
+    # power
+    power_in_ps = -1
+    if 'power' in car['mobile']['technical']:
+        power_raw = car['mobile']['technical']['power']
+        power_in_ps = int(re.findall(r'\d+', power_raw)[1])
+    else:
+        logging.warning('no power found')
+    row['power_ps'] = power_in_ps
+
+    # previous owners
+    if 'numberOfPreviousOwners' in car['mobile']['technical']:
+        row['owners'] = int(car['mobile']['technical']['numberOfPreviousOwners'])
+
+    # cubic
+    if 'cubicCapacity' in car['mobile']['technical']:
+        cubic_capacity_raw = car['mobile']['technical']['cubicCapacity'].replace('.', '')
+        cubic_capacity = int(re.findall(r'\d+', cubic_capacity_raw)[0])
+        row['cubic_capacity'] = cubic_capacity
+    else:
+        logging.warning('no cubic capacity found')
+
+    # inlining features
+    if car['mobile']['features'] is not None:
+        for feature in car['mobile']['features']:
+            row['features_' + feature] = True
+    else:
+        row['feature_none'] = True
+
+    # tech details as strings
+    excluded_keys = ['mileage', 'firstRegistration', 'hu', 'power', 'cubicCapacity', 'numberOfPreviousOwners']
+    row.update({k: v for k, v in car['mobile']['technical'].items() if k not in excluded_keys})
+
+    return row
 
 
 def generate_tree_visualization(regr, vec):
